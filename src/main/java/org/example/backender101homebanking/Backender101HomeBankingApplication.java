89cf9f3f6692817ddbf646e6fbb3f79009530134
mirkemme <mirkomontaina@gmail.com>
2024-01-12 10:09:43 +0100

    project init

---
+++ src/main/java/org/example/backender101homebanking/Backender101HomeBankingApplication.java
@@ -0,0 +1,13 @@
+package org.example.backender101homebanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Backender101HomeBankingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Backender101HomeBankingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/backender101homebanking/Backender101HomeBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.backender101homebanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Backender101HomeBankingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
